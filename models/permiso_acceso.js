86213ddc060333547da14e30f518aae6d3a17738
Anderson Jimenez <andersonmurdog@gmail.com>
2019-05-29 19:47:40 -0500

    se creo el post de guardado de los permisos de uso de salas con cada usuario se vincula lasala con el usuario y se registra si es entrada o salida

--- models/permiso_acceso.js
+++ models/permiso_acceso.js
@@ -14,6 +14,11 @@ let PermisoSchema = new Schema({
     ref:"User",
     required:[true,'Deberia ser asignado']
   },
+  sala:{
+    type:Schema.Types.ObjectId,
+    ref:"Sala",
+    required:[true,'Deberia ser asignado']
+  },
   typeAccess: {
     type: String,
     enum: typeAccess
--- routes/permiso_acceso.js
+++ routes/permiso_acceso.js
@@ -5,6 +5,11 @@ const date = require('date-and-time');
 const dateFormat = require('dateformat');
 const Sala = require('../models/sala');
 
+
+const mongoose = require('mongoose');
+// variable de date
+let now = new Date();
+
 app.get('/permiso',(req,res)=>{
   PermisoAcceso.find({state:true}).exec((err,permisos)=>{
     if(err){
@@ -30,6 +35,61 @@ app.get('/permiso',(req,res)=>{
 
 app.post('/permiso',(req,res)=>{
   let body = req.body
+
+  let permiso_guardar_entrada = new PermisoAcceso({
+    date:dateFormat(now,"dddd, d 'de' mmmm, yyyy"),
+    // la A es la region
+    hour:date.format(now,"hh:mm:ss A"),
+    user: body.user,
+    sala: body.sala,
+    typeAccess: "ENTRADA"
+  })
+
+  let permiso_guardar_salida = new PermisoAcceso({
+    date:dateFormat(now,"dddd, d 'de' mmmm, yyyy"),
+    // la A es la region
+    hour:date.format(now,"hh:mm:ss A"),
+    user: body.user,
+    sala: body.sala,
+    typeAccess: "SALIDA"
+  })
+
+  // El findOne trae solo un dato
+  PermisoAcceso.findOne((
+    {user:body.user}
+  ),(err,result)=>{
+    if(err){
+      return res.status(500).json({
+        ok:false,
+        err
+      })
+    }
+
+    if(result === null){
+      permiso_guardar_entrada.save();
+      return res.status(200).json({
+        ok:true,
+        acceso:permiso_guardar_entrada
+      })
+    }else{
+      if(result.typeAccess === 'SALIDA'){
+        permiso_guardar_entrada.save();
+        return res.status(200).json({
+          ok:true,
+          acceso:permiso_guardar_entrada
+        })
+      }else {
+        if(result.typeAccess === 'ENTRADA')
+        permiso_guardar_salida.save();
+        return res.status(200).json({
+          ok:true,
+          acceso:permiso_guardar_salida
+        })
+      }
+    }
+  }).sort({
+    _id: -1 // nos tare el ultimo registro ordenado desencedntemente
+  });
 });
 
 module.exports = app
