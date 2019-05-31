7b04e994ebf08cd15171ec4f8138292c4d8d3b3f
Anderson Jimenez <andersonmurdog@gmail.com>
2019-05-31 09:41:10 -0500

    cambio menor de correccion en url de base de datos mongo_uri

--- config/config.js
+++ config/config.js
@@ -9,7 +9,7 @@
 // configuracion para DB
 let urlDB;
 // es una irl deun gestio de base gratuito https://cloud.mongodb.com/v2/5cef2bdf9ccf6416a17691e0#clusters?fastPoll=true
-// urlDB = 'mongodb://admin:admin123@ds263816.mlab:63816/sga2'
+// urlDB = 'mongodb://admin:admin123@ds263816.mlab.com:63816/sga2'
 
 if(process.env.ENV == 'dev'){
   urlDB = 'mongodb://localhost:27017/sga2' // 2017 es un puerto x defecto
@@ -25,4 +25,4 @@ process.env.PORT = process.env.PORT || 3500;
 
 // ocupamos apra que jwt funcione
 process.env.SEED = process.env.SEED || "jwtsecretdev" // jwtsecretdev sera la palabra firma
-process.env.CADUCIDAD = process.env.CADUCIDAD || "1d" // definimos el tiempo de expiracion 
+process.env.CADUCIDAD = process.env.CADUCIDAD || "1d" // definimos el tiempo de expiracion
